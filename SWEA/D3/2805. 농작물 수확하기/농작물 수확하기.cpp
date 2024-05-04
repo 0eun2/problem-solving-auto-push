8e3ae6ba808df32b9167e8b32f653400ad36667e
Youngeun Choi <y_e_99@ewhain.net>
2024-05-05 08:55:13 +0900

    [D3] Title: 농작물 수확하기, Time: 22 ms, Memory: 13,556 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.cpp
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<vector>
-
+ 
 using namespace std;
-
-int calculate(int n, vector<vector<int>> v) {
+ 
+int calculate(int n, vector<vector<int>> &v) {
     int ans = 0;
     for (int i = 0; i < n; i++) {   // 0 ~ n-1 행
         int num = abs(n / 2 - i);   // 현재 행과 중앙 행의 차이
@@ -13,18 +13,18 @@ int calculate(int n, vector<vector<int>> v) {
     }
     return ans;
 }
-
+ 
 int main() {
     int T;
     cin >> T;
-
+ 
     for (int test_case = 1; test_case <= T; ++test_case) {
         int n, ans;  // 농장의 크기, 수익
-
+ 
         // 입력
         cin >> n;
         vector<vector<int>> v(n, vector<int>(n, 0));
-
+ 
         for (int i = 0; i < n; i++) {
             string s;
             cin >> s;
@@ -32,7 +32,7 @@ int main() {
                 v[i][j] = s[j] - '0';
             }
         }
-
+ 
         // 출력
         cout << '#' << test_case << ' ' << calculate(n, v) << '\n';
     }
