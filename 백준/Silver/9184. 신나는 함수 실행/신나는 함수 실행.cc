ed5b584275e78c987ed3c574e77f90ae824de4e2
Youngeun Choi <y_e_99@ewhain.net>
2024-05-12 12:06:36 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 440 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -6,28 +6,25 @@ using namespace std;
 vector<vector<vector<int>>> dp;
 
 int w(int a, int b, int c) {
-    int a_idx = a + 50, b_idx = b + 50, c_idx = c + 50;
-    if (dp[a_idx][b_idx][c_idx]) // 값이 존재하면
-        return dp[a_idx][b_idx][c_idx];
-    else if (a <= 0 || b <= 0 || c <= 0) {
-        dp[a_idx][b_idx][c_idx] = 1;
+    if (a <= 0 || b <= 0 || c <= 0) {   // 인덱스에 음수가 들어갈 수 없음
         return 1;
-    } else if (a > 20 || b > 20 || c > 20) {
-        a_idx = 70, b_idx = 70, c_idx = 70;
-        dp[a_idx][b_idx][c_idx] = w(20, 20, 20);
+    } else if (a > 20 || b > 20 || c > 20) {    // 인덱스에 21 이상이 들어갈 수 없음
+        return w(20, 20, 20);
+    } else if (dp[a][b][c]) { // 값이 존재하면 아래 재귀 생략
+        return dp[a][b][c];
     } else if (a < b && b < c) {
-        dp[a_idx][b_idx][c_idx] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
     } else {
-        dp[a_idx][b_idx][c_idx] = w(a - 1, b, c) + w(a - 1, b - 1, c)
-                                  + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c)
+                      + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
     }
 
-    return dp[a_idx][b_idx][c_idx];
+    return dp[a][b][c];
 }
 
 int main() {
     int a, b, c;
-    dp.assign(101, vector<vector<int>>(101, vector<int>(101, 0)));
+    dp.assign(21, vector<vector<int>>(21, vector<int>(21, 0)));
 
     while (true) {
         cin >> a >> b >> c;
