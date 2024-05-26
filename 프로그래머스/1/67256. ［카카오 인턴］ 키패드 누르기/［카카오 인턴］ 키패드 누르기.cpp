dcd4469eb0d1dfa5bdd7965e784a58c12ba5d88a
Youngeun Choi <y_e_99@ewhain.net>
2024-05-27 01:48:15 +0900

    Update ［카카오 인턴］ 키패드 누르기.cpp

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
@@ -3,32 +3,11 @@
 
 using namespace std;
 
-int calcDistance(int key, int hand){
-    int diff = abs(key - hand);
-    switch(diff){
-        case 1:
-        case 3:
-            return 1;
-        case 2:
-        case 4:
-        case 6:
-            return 2;
-        case 5:
-        case 7:
-        case 9:
-            return 3;
-        case 8:
-        case 10:
-            return 4;
-    }
-}
-
 string solution(vector<int> numbers, string hand) {
     string answer = "";
-    int l_hand = 10, r_hand = 12;
+    int l_hand = 10, r_hand = 12;   // 왼손: *, 오른손: # 에서 시작
     
     for(int key: numbers){
-        cout << "left: " << l_hand << '\n' << "right: " << r_hand << '\n';
         if(key == 1 || key == 4 || key == 7){ // 왼손
             l_hand = key;
             answer += 'L';
@@ -37,13 +16,14 @@ string solution(vector<int> numbers, string hand) {
             r_hand = key;
             answer += 'R';
         }
-        else if(key == 2 || key == 5 || key == 8 || key == 0){  // 거리 계산
+        else {  // 거리 계산
             if(key == 0){ // 0은 11로 계산
                 key = 11;
             }
             
-            int l_distance = calcDistance(key, l_hand);
-            int r_distance = calcDistance(key, r_hand);
+                            // 상하                   좌우
+            int l_distance = abs(key - l_hand) / 3 + abs(key - l_hand) % 3;
+            int r_distance = abs(key - r_hand) / 3 + abs(key - r_hand) % 3;
             
             if(l_distance < r_distance){    // 왼손이 더 가까움
                 l_hand = key;
