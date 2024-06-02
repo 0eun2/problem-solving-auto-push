d74c3daffe8511f5f8c5c1d0bf7ea52be34af85e
Youngeun Choi <y_e_99@ewhain.net>
2024-06-02 23:06:07 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -7,9 +7,10 @@ string solution(string number, int k) {
     string answer = "";
     int idx = -1;
 
-    for (int i = 0; i < number.size() - k; i++) {
+    // number.length() - k 수 뽑기
+    for (int i = 0; i < number.length() - k; i++) {
         char max_num = '0';
-        for (int j = idx + 1; j <= k + i; j++) {
+        for (int j = idx + 1; j <= k + i; j++) {    // 가장 큰 수 찾기
             if (max_num < number[j]) {
                 max_num = number[j];
                 idx = j;
