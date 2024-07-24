fd8c67c9eaafeb173de9abcfb4f290c25fd25dd4
Youngeun Choi <youngeun.choi26@gmail.com>
2024-07-25 01:30:52 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,10 +1,3 @@
 function solution(a, b) {
-    var answer = 0;
-    let num1 = Number(a.toString() + b.toString());
-    let num2 = Number(b.toString() + a.toString());
-        
-    if(num1 >= num2) answer = num1;
-    else answer = num2;
-    
-    return answer;
+    return Math.max(`${a}${b}`, `${b}${a}`);
 }
