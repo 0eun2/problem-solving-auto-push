b19bc76723bfc3d306328149f334b166d14b804b
Youngeun Choi <youngeun.choi26@gmail.com>
2024-07-24 21:42:54 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,11 +1,3 @@
 function solution(num_list) {
-    let cnt = 5;
-    
-    num_list.sort((a, b) => a - b);
-    
-    while(cnt--){
-        num_list.shift();
-    }
-    
-    return num_list;
+    return num_list.sort((a, b) => a - b).slice(5);
 }
