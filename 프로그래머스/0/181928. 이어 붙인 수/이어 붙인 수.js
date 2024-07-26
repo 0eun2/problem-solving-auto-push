8dd7c9fc4d04dd05dae78ec05222c50a1176abcc
Youngeun Choi <youngeun.choi26@gmail.com>
2024-07-26 20:48:25 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,4 +1,5 @@
 function solution(num_list) {
-    return Number(num_list.filter((num) => num % 2).join('')) 
-            + Number(num_list.filter((num) => !(num % 2)).join(''));
+    let odd = Number(num_list.filter((num) => num % 2).join(''));
+    let even = Number(num_list.filter((num) => !(num % 2)).join(''));
+    return odd + even; 
 }
