87e146abd5e3bf85fc7c0d012514425bd6e31105
Youngeun Choi <youngeun.choi26@gmail.com>
2024-07-28 20:53:38 +0900

    [level 0] Title: 접미사 배열, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,9 +1,5 @@
 function solution(my_string) {
-    var answer = [];
-
-    for (let i = 0; i < my_string.length; i++){
-        answer.push(my_string.slice(-1 * i));
-    }
-        
-    return answer.sort();
+    return Array.from(my_string)
+        .map((_, i) => my_string.substring(i))
+        .sort();
 }
