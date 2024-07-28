7dcf77a59f5359c030d4629eb0ddc83faff33886
Youngeun Choi <youngeun.choi26@gmail.com>
2024-07-28 20:10:18 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,36 +1,24 @@
 function solution(a, b, c, d) {
     const nums = [a, b, c, d];
-    const appear = {}
+    let dice = {}
     
-    for(let i =0;i<4;i++){
-        const num = nums[i];
-        appear[num] = (appear[num] || 0) + 1;
+    for (let i of nums) {
+        dice[i] = (dice[i] || 0) + 1; 
     }
     
-    const count = Math.max(...Object.values(appear));
-    const entries = Object.entries(appear).sort((a,b)=>a[1] - b[1])
+    const entries = Object.entries(dice).sort((a, b) => b[1] - a[1]);
     
-    let answer = 0;
-    switch(count){
-        case 4:
-            answer = 1111 * a;
-            break;
-        case 3:
-            answer = (10 * Number(entries[1][0]) + Number(entries[0][0])) ** 2
-            break;
-        case 2:
-            if(entries.length === 2){
-                answer = (Number(entries[1][0]) + Number(entries[0][0])) * Math.abs(Number(entries[1][0]) - Number(entries[0][0]))
-            }
-            if(entries.length === 3){
-                const newEntries = entries.filter((el)=> el[1]===1)
-                answer = Number(newEntries[1][0]) * Number(newEntries[0][0])
-            }
-            break;
+    switch(entries.length){
         case 1:
-            answer = Math.min(...nums);
-            break;
+            return 1111 * a;
+        case 2:
+            const p = Number(entries[0][0]);
+            const q = Number(entries[1][0]);
+            if(entries[0][1] === 3) return (10 * p + q) ** 2;
+            else return (p + q) * Math.abs(p - q);
+        case 3:
+            return Number(entries[1][0]) * Number(entries[2][0]);
+        case 4:
+            return Math.min(...nums);
     }
-    
-    return answer;
 }
