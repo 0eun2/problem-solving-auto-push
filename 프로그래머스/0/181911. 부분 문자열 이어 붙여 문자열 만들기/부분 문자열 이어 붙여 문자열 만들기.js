c65a848738745359b849c771e38e46586c27de21
Youngeun Choi <youngeun.choi26@gmail.com>
2024-07-28 20:28:22 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -1,9 +1,3 @@
 function solution(my_strings, parts) {
-    var answer = '';
-    
-    for (let i = 0; i < parts.length; i++) {
-        answer += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
-    }
-    
-    return answer;
+    return parts.map(([s, e], i) => my_strings[i].substring(s, e + 1)).join('');
 }
