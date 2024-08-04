9af2da2ab6a5fc354720b752dd86ceaeab7d387f
Youngeun Choi <youngeun.choi26@gmail.com>
2024-08-05 07:00:31 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.29 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,10 +1,8 @@
 function solution(n) {
-    let answer = Array.from( {length: n}, () => Array(n).fill(0));
+    let answer = Array.from(Array(n), () => Array(n).fill(0));
     
     for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i == j) answer[i][j] = 1;
-        }
+        answer[i][i] = 1;
     }
     
     return answer;
