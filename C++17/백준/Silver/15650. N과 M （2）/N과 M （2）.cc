5d835f31eea9cc1de97f4f57179195496f6fdfc4
Youngeun Choi <youngeun.choi26@gmail.com>
2024-10-28 17:30:09 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ C++17/백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,33 +1,31 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
-/**
- * permutation 이용한 조합 풀이 (브루트 포스) (0ms)
- */
+int N, M;
+vector<int> v;
 
-int main() {
-    int n, m;
-
-    //입력
-    cin >> n >> m;
-    vector<bool> is_in_num(n + 1, false); //1 ~ n까지의 수에 대한 조합정보(true / false)
-
-    //연산
-    for (int i = 1; i <= m; i++) { //m개의 원소 true로 셋팅
-        is_in_num[i] = true;
+void dfs(int num, int cnt) {
+    // 종료 조건: M개의 수를 모두 선택함
+    if (cnt == M) {
+        for (int i: v)
+            cout << i << ' ';
+        cout << '\n';
+        return;
     }
 
-    do {
-        for (int i = 1; i <= n; i++) {
-            if (is_in_num[i]) { //true라면 사용이므로 출력
-                cout << i << ' ';
-            }
-        }
-        cout << '\n';
-    } while (prev_permutation(is_in_num.begin() + 1, is_in_num.end())); //인덱스 1부터 사용
+    for (int i = num; i <= N; i++) {
+        v.push_back(i);
+        dfs(i + 1, cnt + 1);
+        v.pop_back();
+    }
+}
+
+int main() {
+    cin >> N >> M;
+
+    dfs(1, 0);
 
     return 0;
 }
