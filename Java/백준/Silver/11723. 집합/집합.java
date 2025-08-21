86936a9c76c7bedbce9bdd643dd7a8d455e7a692
Youngeun Choi <youngeun.choi26@gmail.com>
2025-08-21 12:08:48 +0900

    [Silver V] Title: 집합, Time: 1040 ms, Memory: 313332 KB -BaekjoonHub

--- Java/백준/Silver/11723. 집합/집합.java
+++ Java/백준/Silver/11723. 집합/집합.java
@@ -1,43 +1,43 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
 		int m = Integer.parseInt(br.readLine());
-		Set<Integer> set = new HashSet<>();
+		int s = 0;
 
-		while (m-- > 0) {
+		for (int i = 0; i < m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String op = st.nextToken();
-			if (op.equals("all")) {
-				set.clear();
-				for (int i = 1; i <= 20; i++) {
-					set.add(i);
-				}
-			} else if (op.equals("empty")) {
-				set.clear();
-			} else if (op.equals("add")) {
-				set.add(Integer.parseInt(st.nextToken()));
-			} else if (op.equals("remove")) {
-				set.remove(Integer.parseInt(st.nextToken()));
-			} else if (op.equals("check")) {
-				int x = Integer.parseInt(st.nextToken());
-				if (set.contains(x))
-					sb.append(1).append("\n");
-				else
-					sb.append(0).append("\n");
-			} else if (op.equals("toggle")) {
-				int x = Integer.parseInt(st.nextToken());
-				if (set.contains(x))
-					set.remove(x);
-				else
-					set.add(x);
+			int x = 0;
+			if (!op.equals("all") && !op.equals("empty")) {
+				x = Integer.parseInt(st.nextToken());
+			}
+
+			switch (op) {
+			case "add":
+				s |= (1 << (x - 1));
+				break;
+			case "remove":
+				s &= ~(1 << (x - 1));
+				break;
+			case "check":
+				sb.append((s & (1 << (x - 1))) != 0 ? "1" : "0").append("\n");
+				break;
+			case "toggle":
+				s ^= (1 << (x - 1));
+				break;
+			case "all":
+				s = (1 << 20) - 1;
+				break;
+			case "empty":
+				s = 0;
+				break;
 			}
 		}
-		
-		System.out.println(sb);
+
+		System.out.print(sb);
 	}
 }
