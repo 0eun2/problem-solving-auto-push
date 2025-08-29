376cfd0099b9ef84bc6bb400961aa604feeb3a1e
Youngeun Choi <youngeun.choi26@gmail.com>
2025-08-29 12:02:27 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1332 ms, Memory: 123640 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ Java 8 (OpenJDK)/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,38 +1,65 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int[] arr;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
 		int n = Integer.parseInt(br.readLine());
-		Map<Integer, Integer> map = new HashMap<>();
-		
+		arr = new int[n];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if (map.containsKey(num)) {
-				map.put(num, map.get(num) + 1);
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		while (m-- > 0) {
+			int target = Integer.parseInt(st.nextToken());
+			sb.append(searchUpperBound(target) - searchLowerBound(target)).append(" ");
+		}
+
+		System.out.println(sb);
+	}
+
+	static int searchLowerBound(int target) {
+		int l = 0;
+		int r = arr.length;
+		int mid;
+
+		while (l < r) {
+			mid = (l + r) / 2;
+
+			if (arr[mid] < target) {
+				l = mid + 1;
 			} else {
-				map.put(num, 1);
+				r = mid;
 			}
 		}
-		
-		int m = Integer.parseInt(br.readLine());
-		int[] arr = new int[m];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < m; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			arr[i] = num;
+
+		return l;
+	}
+
+	static int searchUpperBound(int target) {
+		int l = 0;
+		int r = arr.length;
+		int mid;
+
+		while (l < r) {
+			mid = (l + r) / 2;
+
+			if (arr[mid] <= target) {
+				l = mid + 1;
+			} else {
+				r = mid;
+			}
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < m; i++) {
-			if (map.containsKey(arr[i]))
-				sb.append(map.get(arr[i]));
-			else 
-				sb.append(0);
-			sb.append(" ");
-		}
-		System.out.println(sb);
+
+		return l;
 	}
 }
