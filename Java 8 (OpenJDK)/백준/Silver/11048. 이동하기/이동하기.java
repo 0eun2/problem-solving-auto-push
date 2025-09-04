033c42569ff1005abe251dde9a784ddaca708fdc
Youngeun Choi <youngeun.choi26@gmail.com>
2025-09-04 16:28:12 +0900

    [Silver II] Title: 이동하기, Time: 440 ms, Memory: 78212 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Silver/11048. 이동하기/이동하기.java
+++ Java 8 (OpenJDK)/백준/Silver/11048. 이동하기/이동하기.java
@@ -10,7 +10,7 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int[][] arr = new int[n][m];
-		int[][] dp = new int[n][m];
+		int[][] dp = new int[n + 1][m + 1];
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -19,22 +19,12 @@ public class Main {
 			}
 		}
 
-		dp[0][0] = arr[0][0];
-
-		for (int i = 1; i < n; i++) {
-			dp[i][0] = dp[i - 1][0] + arr[i][0];
-		}
-
-		for (int i = 1; i < m; i++) {
-			dp[0][i] = dp[0][i - 1] + arr[0][i];
-		}
-
-		for (int i = 1; i < n; i++) {
-			for (int j = 1; j < m; j++) {
-				dp[i][j] = Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1])) + arr[i][j];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1])) + arr[i - 1][j - 1];
 			}
 		}
 
-		System.out.println(dp[n - 1][m - 1]);
+		System.out.println(dp[n][m]);
 	}
 }
