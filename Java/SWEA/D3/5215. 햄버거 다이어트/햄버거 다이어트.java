da675e0919328c8bc5c776dcba4654d899b103be
Youngeun Choi <youngeun.choi26@gmail.com>
2025-09-10 17:41:45 +0900

    [D3] Title: 햄버거 다이어트, Time: 134 ms, Memory: 26,112 KB -BaekjoonHub

--- Java/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ Java/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,54 +1,46 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Solution {
- 
-    static class Pair {
-        int taste;
-        int calories;
- 
-        Pair(int taste, int calories) {
-            this.taste = taste;
-            this.calories = calories;
-        }
-    }
- 
-    static List<Pair> list = new ArrayList<>();
-    static int l = 0;
-    static int ans = 0;
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
- 
-        for (int tc = 1; tc <= T; tc++) {
-            ans = 0;
-            list.clear();
- 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            l = Integer.parseInt(st.nextToken());
- 
-            for (int i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                int taste = Integer.parseInt(st.nextToken());
-                int cal = Integer.parseInt(st.nextToken());
-                list.add(new Pair(taste, cal));
-            }
- 
-            backtracking(0, 0, 0);
-            System.out.println("#" + tc + " " + ans);
-        }
-    }
- 
-    static void backtracking(int idx, int tasteSum, int calSum) {
-        if (calSum > l) return;
-        if (idx == list.size()) {
-            ans = Math.max(ans, tasteSum);
-            return;
-        }
-         
-        backtracking(idx + 1, tasteSum + list.get(idx).taste, calSum + list.get(idx).calories);
-        backtracking(idx + 1, tasteSum, calSum);
-    }
+
+	static class Item {
+		int t;
+		int c;
+
+		Item(int t, int c) {
+			this.t = t;
+			this.c = c;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int l = Integer.parseInt(st.nextToken());
+			Item[] arr = new Item[n];
+
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				arr[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+			// 입력 끝
+			int[] dp = new int[l + 1];
+
+			for (int i = 0; i < n; i++) {
+				for (int j = l; j >= arr[i].c; j--) {
+					dp[j] = Math.max(dp[j], dp[j - arr[i].c] + arr[i].t);
+				}
+			}
+
+			sb.append("#").append(tc).append(" ").append(dp[l]).append("\n");
+		}
+
+		System.out.println(sb);
+	}
 }
