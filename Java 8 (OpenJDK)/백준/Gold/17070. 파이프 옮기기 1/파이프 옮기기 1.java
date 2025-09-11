c5dedf58a4c9795f1a195559caa55e6088927adc
Youngeun Choi <youngeun.choi26@gmail.com>
2025-09-11 10:39:58 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ Java 8 (OpenJDK)/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -2,42 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] arr;
-	static int n, ans = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n][n];
-		
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][n];
+
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j= 0; j < n; j++) {
+			for (int j = 0; j < n; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		dfs(0, 1, 0);
+		// 입력 끝
 
-		System.out.println(ans);
-	}
+		int[][][] dp = new int[n][n][3];
 
-	static void dfs(int r, int c, int dir) { // r, c: 오른쪽 파이프 위치
-		if (r == n - 1 && c == n - 1) {
-			ans++;
-			return;
+		for (int i = 1; i < n; i++) {
+			if (arr[0][i] == 1) {
+				break;
+			}
+			dp[0][i][0] = 1;
 		}
 
-		// 0: 가로, 1: 세로, 2: 대각선
-		if ((dir == 0 || dir == 2) && (c + 1 < n) && (arr[r][c + 1] == 0)) {
-			dfs(r, c + 1, 0);
-		} 
-		if ((dir == 1 || dir == 2) && (r + 1 < n) && (arr[r + 1][c] == 0)) {
-			dfs(r + 1, c, 1);
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				if (arr[i][j] != 1) {
+					dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+					dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+					if (arr[i - 1][j] != 1 && arr[i][j - 1] != 1) {
+						dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+					}
+				}
+			}
 		}
-		if ((r + 1 < n) && (c + 1 < n) && (arr[r + 1][c] == 0) && (arr[r][c + 1] == 0) && (arr[r + 1][c + 1] == 0)) { 
-			dfs(r + 1, c + 1, 2);
-		}
-			
+
+		System.out.println(dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2]);
 	}
 }
