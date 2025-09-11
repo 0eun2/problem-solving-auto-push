cff6ebe8f6ab341ddb8b6c51b136ab875cf9ff88
Youngeun Choi <youngeun.choi26@gmail.com>
2025-09-11 10:54:55 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ Java 8 (OpenJDK)/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -13,9 +13,7 @@ public class Main {
 			for (int j = 0; j < n; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
-		}
-
-		// 입력 끝
+		} // 입력 끝
 
 		int[][][] dp = new int[n][n][3];
 
@@ -28,13 +26,16 @@ public class Main {
 
 		for (int i = 1; i < n; i++) {
 			for (int j = 1; j < n; j++) {
-				if (arr[i][j] != 1) {
-					dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
-					dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
-					if (arr[i - 1][j] != 1 && arr[i][j - 1] != 1) {
-						dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
-					}
-				}
+				if (arr[i][j] == 1)
+					continue;
+
+				dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+				dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+
+				if (arr[i - 1][j] == 1 || arr[i][j - 1] == 1)
+					continue;
+
+				dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
 			}
 		}
 
