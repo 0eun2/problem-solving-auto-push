ff6f541833b39fb8d4926d55a0392eaff7d98481
Youngeun Choi <youngeun.choi26@gmail.com>
2025-10-14 14:31:21 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 592 ms, Memory: 25396 KB -BaekjoonHub

--- Java/백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
+++ Java/백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -15,8 +15,8 @@ public class Main {
 	}
 
 	static int n;
-	static List<FireBall> fireBall;
-	static int[][] map;
+	static ArrayList<FireBall>[][] map;
+	static ArrayList<FireBall> fireBall;
 	static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 };
 	static int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
@@ -27,8 +27,14 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
+		map = new ArrayList[n][n];
 		fireBall = new ArrayList<>();
-		map = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				map[i][j] = new ArrayList<>();
+			}
+		}
 
 		while (m-- > 0) {
 			st = new StringTokenizer(br.readLine());
@@ -36,17 +42,19 @@ public class Main {
 			int c = Integer.parseInt(st.nextToken()) - 1;
 			fireBall.add(new FireBall(r, c, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
 					Integer.parseInt(st.nextToken())));
-			map[r][c]++;
 		} // 입력 끝
 
 		while (k-- > 0) { // 명령
 			move();
 
+			// 파이어볼 분리 & map 초기화
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
-					if (map[i][j] > 1) {
-						divide(i, j);
+					if (map[i][j].size() < 2) {
+						map[i][j].clear();
+						continue;
 					}
+					divide(i, j);
 				}
 			}
 		}
@@ -56,43 +64,34 @@ public class Main {
 
 	static void move() { // 파이어볼 이동
 		for (FireBall f : fireBall) {
-			map[f.r][f.c]--;
 			f.r = (f.r + n + dr[f.d] * (f.s % n)) % n;
 			f.c = (f.c + n + dc[f.d] * (f.s % n)) % n;
-
-			map[f.r][f.c]++;
+			map[f.r][f.c].add(f);
 		}
 	}
 
 	static void divide(int r, int c) { // 파이어볼 분리
-		int m = 0, s = 0, cnt = 0;
+		int m = 0, s = 0, cnt = map[r][c].size();
 		boolean odd = false, even = false;
 
-		for (int i = 0; i < fireBall.size(); i++) {
-			FireBall cur = fireBall.get(i);
-			if (cur.r == r && cur.c == c) {
-				cnt++;
-				m += cur.m;
-				s += cur.s;
-				if (cur.d % 2 == 0)
-					even = true;
-				else
-					odd = true;
+		for (FireBall cur : map[r][c]) {
+			m += cur.m;
+			s += cur.s;
+			if (cur.d % 2 == 0)
+				even = true;
+			else
+				odd = true;
 
-				fireBall.remove(i);
-				i--;
-			}
+			fireBall.remove(cur);
 		}
+		map[r][c].clear();
 
 		m /= 5;
-		if (m == 0) {
-			map[r][c] = 0;
+		if (m == 0) { // 소멸
 			return;
 		}
 
 		s /= cnt;
-
-		map[r][c] = 4;
 
 		if (odd && even) {
 			for (int i = 1; i < 8; i += 2) {
