948c5a09704765ee98b9103a380f146b3eae28a8
Youngeun Choi <youngeun.choi26@gmail.com>
2025-10-30 11:51:31 +0900

    [Silver II] Title: A → B, Time: 60 ms, Memory: 11532 KB -BaekjoonHub

--- Java/백준/Silver/16953. A → B/A → B.java
+++ Java/백준/Silver/16953. A → B/A → B.java
@@ -17,47 +17,22 @@ public class Main {
 
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
+		int ans = 1;
 
-		System.out.println(bfs(a, b));
-	}
-
-	static int bfs(int a, int b) {
-		if (a == b)
-			return 1;
-
-		if (b % 2 != 0 && b % 10 != 1)
-			return -1;
-
-		PriorityQueue<Pair> pq = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.cnt, p2.cnt));
-
-		pq.add(new Pair(b, 1)); // 역추적
-
-		while (!pq.isEmpty()) {
-			Pair cur = pq.poll();
-
-			int next;
-			if (cur.num % 2 == 0) {
-				next = cur.num / 2;
-
-				if (next == a)
-					return cur.cnt + 1;
-				if (next < a)
-					continue;
-
-				pq.add(new Pair(next, cur.cnt + 1));
+		while (a != b) {
+			if (a > b || (b % 2 != 0 && b % 10 != 1)) {
+				ans = -1;
+				break;
 			}
 
-			if (cur.num % 10 == 1) {
-				next = cur.num / 10;
+			if (b % 2 == 0)
+				b /= 2;
+			else
+				b /= 10;
 
-				if (next == a)
-					return cur.cnt + 1;
-				if (next < a)
-					continue;
-
-				pq.add(new Pair(next, cur.cnt + 1));
-			}
+			ans++;
 		}
-		return -1;
+
+		System.out.println(ans);
 	}
 }
