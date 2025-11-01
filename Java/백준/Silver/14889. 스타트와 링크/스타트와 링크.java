d2288270e19456451e9d5f285637842e3a27ad9c
Youngeun Choi <youngeun.choi26@gmail.com>
2025-11-02 02:05:00 +0900

    [Silver I] Title: 스타트와 링크, Time: 340 ms, Memory: 14520 KB -BaekjoonHub

--- Java/백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ Java/백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,62 +1,61 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int n, ans = Integer.MAX_VALUE;
-	static int s[][];
-	static boolean team[];
+	static int[][] arr;
+	static boolean[] team;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		s = new int[n][n];
+		arr = new int[n][n];
 		team = new boolean[n];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
-				s[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
-		}
+		} // 입력 끝
 
 		dfs(0, 0);
-
+		
 		System.out.println(ans);
 	}
 
 	static void dfs(int idx, int cnt) {
+		if (ans == 0)
+			return;
+		
 		if (cnt == n / 2) {
-			List<Integer> start = new ArrayList<>();
-			List<Integer> link = new ArrayList<>();
+			int start = 0, link = 0;
 			for (int i = 0; i < n; i++) {
-				if (team[i])
-					start.add(i);
-				else
-					link.add(i);
+				if (team[i]) {
+					start += calcSum(i);
+				} else {
+					link += calcSum(i);
+				}
 			}
-			ans = Math.min(ans, calc(start, link));
+			ans = Math.min(ans, Math.abs(start - link));
 			return;
 		}
 
 		for (int i = idx; i < n; i++) {
-			if (team[i] != true) {
-				team[i] = true;
-				dfs(i, cnt + 1);
-				team[i] = false;
-			}
+			team[i] = true;
+			dfs(i + 1, cnt + 1);
+			team[i] = false;
 		}
 	}
 
-	static int calc(List<Integer> start, List<Integer> link) {
-		int sSum = 0, lSum = 0;
-
-		for (int i = 0; i < n / 2; i++) {
-			for (int j = 0; j < n / 2; j++) {
-				sSum += s[start.get(i)][start.get(j)];
-				lSum += s[link.get(i)][link.get(j)];
-			}
+	static int calcSum(int num) {
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			if (num == i)
+				continue;
+			if (team[num] == team[i])
+				sum += arr[num][i];
 		}
-
-		return Math.abs(sSum - lSum);
+		return sum;
 	}
 }
