69036d90b023cb0361771889bcdca2111c4ac9ec
Youngeun Choi <youngeun.choi26@gmail.com>
2025-11-05 15:58:30 +0900

    [D2] Title: 나무 높이, Time: 87 ms, Memory: 26,112 KB -BaekjoonHub

--- Java/SWEA/D2/14510. 나무 높이/나무 높이.java
+++ Java/SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,11 +1,11 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
 		int t = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= t; tc++) {
@@ -17,27 +17,29 @@ public class Solution {
 			for (int i = 0; i < n; i++) {
 				tree[i] = Integer.parseInt(st.nextToken());
 				maxTree = Math.max(maxTree, tree[i]);
-			}
-
+			}	// 입력 끝
+			
 			int odd = 0, even = 0;
-			for (int i : tree) {
-				int diff = maxTree - i;
+			
+			for (int i = 0; i < n; i++) {
+				int diff = maxTree - tree[i];
 				odd += diff % 2;
 				even += diff / 2;
 			}
-
+			
+			// 홀수, 짝수날 맞추기
 			if (even > odd) {
 				while (Math.abs(even - odd) > 1) {
-					odd += 2;
 					even--;
+					odd += 2;
 				}
 			}
-
+			
 			int ans = 0;
-			if (even >= odd) {
-				ans = even * 2;
-			} else if (even < odd) {
+			if (odd > even) {
 				ans = odd * 2 - 1;
+			} else {
+				ans = even * 2;
 			}
 
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
@@ -45,4 +47,5 @@ public class Solution {
 
 		System.out.println(sb);
 	}
+
 }
